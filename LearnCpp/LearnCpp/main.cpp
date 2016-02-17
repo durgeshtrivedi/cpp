e5d6e2404495132106c656aea39cbcf667d3376e
Durgesh Trivedi <durgesh.trivedi@synerzip.com>
2016-02-17 16:21:29 +0530

    just started cpp

---
+++ LearnCpp/LearnCpp/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  LearnCpp
+//
+//  Created by Durgesh Trivedi on 17/02/16.
+//  Copyright © 2016 durgesh. All rights reserved.
+//
+
+#include <iostream>
+
+//int main(int argc, const char * argv[]) {
+//    // insert code here...
+//    std::cout << "Hello, World!\n";
+//    return 0;
+//}
+
+int main()
+
+{
+    using namespace std;
+    cout << "Hello this is a cpp program after long time.\n";
+    return 0;
+}
+
+void addthenumber()
+{
+    using namespace std;
+    cout<<"Please inter ther number you want to add";
+    cin >>;
+}
